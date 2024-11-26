46425811a0d2ecf71364debde30c04c7fc292515
GavinS <gavinslusher@gmail.com>
2024-11-26 17:10:31 -0500

    add nvim kickstart to install script

--- install.sh
+++ install.sh
@@ -7,7 +7,13 @@
 mkdir -p "$HOME/.config/nvim" 
 mkdir -p "$HOME/.config/nvim/undo" 
 
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+ln -sf "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+ln -s "$HOME/dotfiles/nvim/lua" "$HOME/.config/nvim"
+ln -s "$HOME/dotfiles/nvim/doc" "$HOME/.config/nvim"
+
+########
+# X11 #
+########
 
 rm -rf "$HOME/.config/X11" 
 ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
